1ac11ce973edbe6563a083217fa478a773fb78f3
Piyush Garg <piyushgarg2101@gmail.com>
2025-10-12 10:54:39 +0530

    fix: update CORS origin to an array format and add start script to package.json

--- ExpenSaves/Server Side/src/app.js
+++ ExpenSaves/Server Side/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173"],
     credentials: true
 }));
 app.use(express.json());
