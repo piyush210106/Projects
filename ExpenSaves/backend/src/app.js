26a7fbb479fc1a918eec529cefee97a82008cba8
Piyush Garg <piyushgarg2101@gmail.com>
2025-10-12 12:35:18 +0530

    fix: refactor CORS configuration to use a variable for improved readability

--- ExpenSaves/backend/src/app.js
+++ ExpenSaves/backend/src/app.js
@@ -6,12 +6,13 @@ import authRouter from "./routes/auth.route.js";
 import transactionRouter from "./routes/transaction.route.js";
 
 const app = express();
-
+const corsOptions = {
+  origin: "http://localhost:5173",  
+  credentials: true
+};
 app.use(cookieParser());
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true
-}));
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
