c42dc1e705bace47ab13d7563f52b4d4236c0b49
Piyush Garg <piyushgarg2101@gmail.com>
2026-03-12 08:32:33 +0530

    refactor: Update Firebase initialization to use environment variables for credentials

--- InterVue/Server Side/config/firebase.config.js
+++ InterVue/Server Side/config/firebase.config.js
@@ -1,11 +1,13 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
+import dotenv from "dotenv";
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebaseServiceAccount.json");
-
+dotenv.config();
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
   storageBucket: "gs://intervue-1406a.firebasestorage.app",
 });
 
